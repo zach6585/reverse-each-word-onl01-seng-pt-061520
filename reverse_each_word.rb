8364aab62e3d310983576b18089d3a740e0b64ea
zachary mines <zmines@wesleyan.edu>
2020-06-22 18:39:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,18 @@ def reverse_each_word(text)
 end 
 
 
+# def reverse_each_word(text)
+#   a = text.split
+#   b = ""
+#   i = 0
+#   a.each do |word| 
+#     if i != 0
+#       b = b + " " + word.reverse 
+#     else 
+#       b = b + word.reverse
+#     i +=1
+#     end 
+#   end
+#   return b
+  
+# end 
